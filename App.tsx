e23a22eec7200e60ba2d5cdc52ece1aa541b6244
Tonatiuh Rodriguez <34406649+TCRodriguez@users.noreply.github.com>
2024-07-25 15:58:42 -0700

    initial components and styling system

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import GamesScreen from './src/screens/Games';
+import SettingsScreen from './src/screens/Settings';
+import { theme } from './src/styles/theme';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    < NavigationContainer >
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: '#333' },
+          headerStyle: { backgroundColor: '#333' },
+          headerTitleAlign: 'center',
+        }}
+      >
+        <Tab.Screen name="Games" component={GamesScreen} />
+        <Tab.Screen
+          name="Settings"
+          // options={{
+          //   headerTitle: () => (
+          //     <Image
+          //       source={require('./assets/Training_Mode_Logo.png')}
+          //       style={{ width: 40, height: 40 }}
+          //     />
+          //   )
+          // }}
+          component={SettingsScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer >
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: theme.colors['apex-blue'],
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/components/GlobalText.tsx
@@ -0,0 +1,9 @@
+
+import styled from 'styled-components/native';
+import { theme } from '../styles/theme';
+
+const GlobalText = styled.Text`
+    color: ${theme.colors['white']};
+`;
+
+export default GlobalText;
