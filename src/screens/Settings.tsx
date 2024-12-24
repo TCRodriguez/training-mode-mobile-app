fc376d625674d40ad56db0380e4ecda159085620
Tonatiuh Rodriguez <34406649+TCRodriguez@users.noreply.github.com>
2024-12-24 12:23:11 -0800

    add code to test rendering game list on settings page;

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -6,34 +6,35 @@ import GlobalText from '../components/GlobalText';
 import { drizzle } from 'drizzle-orm/expo-sqlite';
 import { openDatabaseSync } from 'expo-sqlite/next';
 import { games } from '../db/schemas/gamesTableSchema';
+import { deviceDBInit } from '../helpers/deviceDBHelpers';
 
 
 
 const deviceDBName = process.env.EXPO_PUBLIC_DEVICE_DATABASE_NAME || 'default.db';
 
-const expoDb = openDatabaseSync(`${deviceDBName}.db`); // Ensure the name matches your copied file
-const db = drizzle(expoDb);
+// const expoDb = openDatabaseSync(`${deviceDBName}.db`); // Ensure the name matches your copied file
+// const db = await deviceDBInit();
 const SettingsScreen = () => {
 
   const [gamesData, setGamesData] = useState<any[]>([]);
 
   // Function to fetch games from the database
   const fetchGames = async () => {
-    try {
-      const results = await db.select().from(games);
-
-      const formattedGames = results.map((game) => {
-        // Return only the necessary fields, excluding created_at and updated_at
-        const { created_at, updated_at, ...rest } = game;
-        return rest;
-      });
-
-      console.log(formattedGames);
-      setGamesData(formattedGames);
-
-    } catch (error) {
-      console.error('Error fetching games:', error);
-    }
+    // try {
+    //   const results = await db.select().from(games);
+    //
+    //   const formattedGames = results.map((game) => {
+    //     // Return only the necessary fields, excluding created_at and updated_at
+    //     const { created_at, updated_at, ...rest } = game;
+    //     return rest;
+    //   });
+    //
+    //   console.log(formattedGames);
+    //   setGamesData(formattedGames);
+    //
+    // } catch (error) {
+    //   console.error('Error fetching games:', error);
+    // }
   };
 
   // Fetch games on component mount
